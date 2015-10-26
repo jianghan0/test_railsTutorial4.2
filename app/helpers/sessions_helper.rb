5421375cc05bce40a3438eda9390a36c652495f6
jianghan0 <jh.shuai@gmail.com>
2015-10-26 11:15:33 +0900

    finish 8.2.3

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,16 @@
+module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+
+  #現在ログインしているユーザーを返す（ユーザーがログイン中の場合のみ）
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #ユーザーがログインしていればtrue
+  def logged_in?
+    !current_user.nil?
+  end
+end
