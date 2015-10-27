5c6277a5ca34eb6588db494dee3654bd945b15b5
jianghan0 <jh.shuai@gmail.com>
2015-10-28 00:39:39 +0900

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,10 +3,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
 
   #現在ログインしているユーザーを返す（ユーザーがログイン中の場合のみ）
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   #ユーザーがログインしていればtrue
@@ -14,8 +28,17 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  #永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
 end
