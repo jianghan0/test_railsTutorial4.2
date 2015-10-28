61f2b728c8971ae18f3bbfbe2fcc95db9326a839
jianghan0 <jh.shuai@gmail.com>
2015-10-28 09:03:42 +0900

    add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,15 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
-end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 
 private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+
+end
